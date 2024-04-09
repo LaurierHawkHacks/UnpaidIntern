2189153fa6024a25269132ecc037a77d89269485
Nausher Rao <SherRao15@gmail.com>
2024-04-09 04:35:06 -0400

    idk what to write for this commit but it works 100%

--- src/index.js
+++ src/index.js
@@ -1,87 +1,84 @@
-import { Client, Events, GatewayIntentBits } from "discord.js";
-import { initializeApp as initFirebase } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { Client, Events, EmbedBuilder, GatewayIntentBits } from "discord.js";
 import { initializeApp as initFirebaseAdmin } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
 import { getAuth } from "firebase-admin/auth";
+import { cert } from "firebase-admin/app";
+
+import { gcpServiceAccountConfig } from "./config.js";
 import 'dotenv/config';
 
 const TOKEN = process.env.DISCORD_BOT_TOKEN;
 const SERVER_ID = process.env.DISCORD_SERVER_ID;
 const CHANNEL_ID = process.env.DISCORD_CHANNEL_ID;
 const VP_CHANNEL_ID = process.env.DISCORD_VP_CHANNEL_ID;
-
-const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-};
-
-let client;
-let firebase;
-let firebaseAdmin;
-let firestore;
-let auth;
+const UPDATE_INTERVAL = 60000;
+let lastUpdateAutomatic = true;
 
 const main = async () => {
-    init();
     client.once(Events.ClientReady, async (readyClient) => {
         console.log("🚀 UnpaidIntern v2 is online!");
-        // try {
-            const channel = readyClient.guilds.cache.get(SERVER_ID).channels.cache.get(CHANNEL_ID);
-            const message = await createMessage();
-            setInterval(() => {
-                if (channel.messages.cache.size === 0) {
-                    channel.send("Hello, world!").then(sentMessage => {
-                        message = sentMessage;
-                    });
-                } else {
-                    message.edit("Hello, world!");
-                }
-            }, 60000);
-
-        // } catch (err) {
-            // const vpChannel = readyClient.guilds.cache.get(SERVER_ID).channels.cache.get(VP_CHANNEL_ID);
-            // vpChannel.send(`❌ Client.onceReady: An error occurred: ${error}`);
-        // }
+        updateMessage();
     } );
 
-    // client.on("error", error => {
-    //     const vpChannel = client.guilds.cache.get(SERVER_ID).channels.cache.get(VP_CHANNEL_ID);
-    //     vpChannel.send(`❌ Client.onError: An error occurred: ${error}`);
-    //     console.error(error);
-    // });
+    client.on(Events.Error, err => {
+        const vpChannel = client.guilds.cache.get(SERVER_ID).channels.cache.get(VP_CHANNEL_ID);
+        vpChannel.send(`❌ Client.onError: An error occurred: ${err}`);
+        console.log("❌ Client.onError: An error occurred!");
+        console.error(err);
+        process.exit(1);
+    });
 
     client.login(TOKEN);
 };
 
 const init = () => {
-    client = new Client({ intents: [GatewayIntentBits.Guilds] });
-    firebase = initFirebase(firebaseConfig);
-    firestore = getFirestore(firebase);
-    firebaseAdmin = initFirebaseAdmin();
-    auth = getAuth(firebaseAdmin);
+    const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+    const firebase = initFirebaseAdmin({ credential: cert(gcpServiceAccountConfig) });
+    const firestore = getFirestore(firebase);
+    const auth = getAuth(firebase);
+
+    return [client, firebase, firestore, auth];
+}
+
+const updateMessage = async () => {
+    const channel = client.guilds.cache.get(SERVER_ID).channels.cache.get(CHANNEL_ID);
+    const [message, apps, users] = await createMessage();
+    if (channel.messages.cache.size === 0)
+        channel.send({ embeds: [message] });
+
+    else
+        channel.messages.cache.first().edit({ embeds: [message] });
+
+    console.log(`🔄 Updated message at ${new Date().toLocaleString()}, apps=${apps}, users=${users}`);
+    setTimeout(updateMessage, UPDATE_INTERVAL);
 }
 
 const createMessage = async () => {
     const applicationCount = await getApplicationCount();
     const userCount = await getRegisteredUserCount();
-    const message = `📊 **Statistics**\n\n👥 Registered Users: ${userCount}\n📝 Applications: ${applicationCount}`;
-    return message;
+    return [
+        new EmbedBuilder()
+            .setTitle("📊 **Statistics**")
+            .setDescription(`Updates automatically once every ${UPDATE_INTERVAL / 1000} seconds.`)
+            .addFields(
+                { name: "👥 Registered Users", value: `${userCount}`, inline: true },
+                { name: "📝 Applications", value: `${applicationCount}`, inline: true } )
+            .setFooter({ text: `Last Updated: ${new Date().toLocaleString()} ${lastUpdateAutomatic ? "(auto)" : "(manual)"}` }),
+        applicationCount,
+        userCount
+    ];
 };
 
 const getApplicationCount = async () => {
-    const docRef = collection(firestore, "applications");
-    const snapshot = await getDocs(docRef);
-    return snapshot.size
+    const collectionRef = firestore.collection("applications");
+    const snapshot = await collectionRef.get();
+    return snapshot.size;
 };
 
 const getRegisteredUserCount = async () => {
-    const users = await auth.listUsers({ maxResults: 1000 });
+    const users = await auth.listUsers(1000);
     return users.users.length;
 }
 
+const [client, firebase, firestore, auth] = init();
 main();
